179dc1b753306313e3518a5069e48618d56219bb
toure <lamagniguitoure@gmail.com>
2019-11-04 23:02:46 +0000

    implementation de graphene
    implementation de graphene avec mutatiion

--- graphene/django_graphql_movies/django_graphql_movies/settings.py
+++ graphene/django_graphql_movies/django_graphql_movies/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_admin_generator',
     'graphene_django',
     'graphene',
+
     
 ]
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'django_graphql_movies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
